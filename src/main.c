b7047a1978787648c7c79792e311c8befb2ae4cd
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2023-09-30 22:14:16 +0700

    Добавление ресурсов

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <genesis.h>
+#include "gfx.h"
+
+void init(){
+	    
+    VDP_init();// Инициализация SGDK
+	// Загрузка изображения в VRAM
+     // VDP_drawImageEx(BG_B, &main_bg, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, 0), 0, 0, FALSE, TRUE);
+}
+
+void update(){
+	
+
+	VDP_waitVSync();
+}
 
 int main()
 {
-	VDP_drawText("Hello Sega World!", 10, 13);
+	// VDP_drawText("Hello Sega World!", 10, 13);
+
+	init();
 
 	while(1)
 	{        
-		VDP_waitVSync();
+		update();
 	}
 	return (0);
 }
